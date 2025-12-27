3664eb0132b10b3acfad647252f43a93a9691bf7
Charles Nicholas Kellett <nkellett@deploy.solutions>
2025-12-27 13:55:16 -0500

    Update to .NET 10 and all packages

--- src/Abp/Runtime/Serialization/BinarySerializationHelper.cs
+++ src/Abp/Runtime/Serialization/BinarySerializationHelper.cs
@@ -87,7 +87,7 @@ namespace Abp.Runtime.Serialization
         }
 
         /// <summary>
-        /// TODO@NET9.0 -> See https://learn.microsoft.com/en-us/dotnet/standard/serialization/binaryformatter-migration-guide/
+        /// TODO@net10.0 -> See https://learn.microsoft.com/en-us/dotnet/standard/serialization/binaryformatter-migration-guide/
         /// </summary>
         /// <param name="extended"></param>
         /// <returns></returns>
