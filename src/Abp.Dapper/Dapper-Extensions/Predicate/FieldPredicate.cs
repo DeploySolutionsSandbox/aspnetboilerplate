91a05b4c7689cc29e9565b0bd37f80d79a07278b
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-01-31 12:37:38 -0500

    Remove Oracle from Dapper due to license confusion

--- src/Abp.Dapper/Dapper-Extensions/Predicate/FieldPredicate.cs
+++ src/Abp.Dapper/Dapper-Extensions/Predicate/FieldPredicate.cs
@@ -95,14 +95,14 @@ namespace Abp.Dapper_Extensions.Predicate
             }
 
             string format;
-            
+
             if (Operator == Operator.BitEq && Value != null)
             {
-                format = (sqlGenerator.Configuration.Dialect is OracleDialect) ? "BITAND({0}, {2}) {1} {2}" : "{0}&{2} {1} {2}";
+                format = "{0}&{2} {1} {2}";
             }
             else
             {
-                format = (Operator == Operator.Like && Value != null && sqlGenerator.Configuration.Dialect is OracleDialect) ?
+                format = (Operator == Operator.Like && Value != null) ?
                     "(upper({0}) {1} upper('%'||{2}||'%'))" : "({0} {1} {2})";
             }
 
